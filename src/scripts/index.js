1be1e9440351737f7477443e3836ace2c894c769
shoya <ilmimusthofa8189@gmail.com>
2024-03-28 01:15:47 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,33 @@
+// import "regenerator-runtime"; /* for async await transpile */
+import "./component/list-restorants.js";
+import "../styles/main.scss";
+import $ from "jquery";
+
+function scroll(scroll) {
+  if (scroll > 10) {
+    $("nav").addClass("nav-light");
+    $(".nav-item a").css("color", " rgb(94, 94, 94)");
+  } else {
+    $("nav").removeClass("nav-light");
+    $(".nav-item a").css("color", "white");
+  }
+}
+
+window.addEventListener("scroll", function () {
+  scroll(this.scrollY);
+});
+
+$.getJSON("./data/DATA.json", function (data) {
+  const restaurants = data.restaurants;
+  restaurants.forEach((resto) => {
+    const listResto = $("<list-restorants></list-restorants>").get(0);
+    listResto.resto = resto;
+    $(".mainCard").append(listResto);
+  });
+});
+
+$(".toggle").on("click", () => {
+  const checkBox = $("#checkbox2");
+  checkBox.prop("checked", !checkBox.prop("checked"));
+  $(".nav-list").toggleClass("menu-view").siblings().removeClass("menu-view");
+});
